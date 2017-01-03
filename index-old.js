c41e9c3564e9e132eeca2991d630bb00aafc0a48
Gregory Daynes <gregdaynes@gmail.com>
2017-01-03 13:56:09 -0800

    converted to a class singleton. added connect and close functionality

---
+++ index-old.js
@@ -0,0 +1,245 @@
+const amqp = require('amqplib');
+const http = require('http');
+
+let config = {
+    _constructed: false,
+    user: process.env.RABBITMQ_DEFAULT_USER || null,
+    pass: process.env.RABBITMQ_DEFAULT_PASS || null,
+    host: process.env.RABBITMQ_HOST || null,
+    ssl: process.env.RABBITMQ_SSL_ENABLED || false,
+    port: process.env.RABBITMQ_PORT || 5672,
+    adminPort: process.env.RABBITMQ_ADMIN_PORT || 15672,
+    queues: [],
+    exchanges: [],
+    policies: [],
+};
+
+exports = module.exports = createRMQ.rmq = createRMQ.default = createRMQ;
+
+function createRMQ(cfg) {
+    const rmq = {};
+    rmq.config = initializeConfig(cfg);
+
+    connection()
+        .then(() => Promise.all([
+            createQueues(),
+            createExchanges(),
+            createPolicies(),
+            createUpstreams(),
+        ]))
+        .then(() => Promise.all([
+            bindQueues(),
+            createConsumers(),
+        ]))
+        .catch(err => { throw err; });
+
+    rmq.channel = createChannel;
+    rmq.connect = createConnection;
+
+    return rmq;
+}
+
+function initializeConfig(cfg) {
+    if (!cfg) return config;
+
+    // setup configuration
+    config = Object.keys(cfg).reduce((acc, key) => {
+        if (key) acc[key] = cfg[key];
+
+        return acc;
+    }, config);
+
+    const protocol = (config.ssl) ? 'amqps://' : 'amqp://';
+    config.url = `${protocol}${config.user}:${config.pass}@${config.host}`;
+
+    config._constructed = true;
+    return config;
+}
+
+function createConnection(options) {
+    return amqp.connect(config.url, options)
+        .then(connection => {
+            config.connection = connection;
+            return connection;
+        })
+        .catch(err => { throw err; });
+}
+
+function createChannel() {
+    return connection()
+        .then(() => config.connection.createChannel())
+        .then(channel => {
+            channel.publishTo = publishTo;
+            channel.queue = queue;
+
+            return channel;
+        })
+        .catch(err => { throw err; });
+}
+
+function connection() {
+    return new Promise((resolve, reject) => {
+        if (config.connection) return resolve(config.connection);
+
+        return resolve(createConnection());
+    });
+}
+
+function createQueues() {
+    return createChannel()
+        .then(channel => Promise.all(config.queues.reduce((acc, q) => {
+            Object.keys(q.actions).forEach(action => {
+                acc.concat([channel.assertQueue(`${q.key}.${action}`, q.actions[action].options)]);
+            });
+
+            return acc;
+        }, [createChannel()])))
+        .then(response => response[0].close())
+        .catch(err => { throw err; });
+}
+
+function createExchanges() {
+    return createChannel()
+        .then(channel => Promise.all(config.exchanges.reduce((acc, x) => {
+            acc.concat([channel.assertExchange(x.key, x.type, x.options)]);
+            return acc;
+        }, [createChannel()])))
+        .then(response => response[0].close())
+        .catch(err => { throw err; });
+}
+
+function createPolicies() {
+    return Promise.all(config.policies.reduce((acc, p) => {
+        acc.concat([createPolicy(p)]);
+        return acc;
+    }, []))
+        .catch(err => { throw err; });
+}
+
+function createUpstreams() {
+    return Promise.all(config.upstreams.reduce((acc, u) => {
+        acc.concat([createUpstream(u)]);
+        return acc;
+    }, []))
+        .catch(err => { throw err; });
+}
+
+function createConsumers() {
+    return createChannel()
+        .then(channel => {
+            config.queues.forEach(q => {
+                Object.keys(q.actions).forEach(action => {
+                    const queue = q.actions[action];
+
+                    if (queue.consume && findExchange(queue.source)) {
+                        channel.consume(`${q.key}.${action}`, (msg) => queue.consume(msg, channel));
+                    }
+                });
+            });
+        })
+        .catch(err => { throw err; });
+}
+
+function findExchange(name) {
+    return config.exchanges.find(obj => obj.name === name);
+}
+
+function bindQueues() {
+    return createChannel()
+        .then(channel => Promise.all(config.queues.reduce((acc, q) => {
+            Object.keys(q.actions).forEach(action => {
+                const queue = q.actions[action];
+
+                const exchange = findExchange(queue.source);
+                const queueAction = `${q.key}.${action}`;
+                const routing = queue.pattern || queue.routingKey;
+
+                if (exchange && !queue.noBind) {
+                    acc.concat([
+                        channel.bindQueue(queueAction, exchange.key, routing),
+                    ]);
+                }
+            });
+
+            return acc;
+        }, [createChannel()])))
+        .then(response => response[0].close())
+        .catch(err => { throw err; });
+}
+
+function createPolicy(policy) {
+    policy.vhost = policy.vhost || '/';
+
+    const options = {
+        method: 'PUT',
+        path: `/api/policies/${encodeURIComponent(policy.vhost)}/${encodeURIComponent(policy.name)}`,
+        auth: `${config.user}:${config.pass}`,
+        hostname: `${config.host}`,
+        port: config.adminPort,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, res => resolve(res));
+        req.on('error', err => reject(err));
+        req.write(JSON.stringify(policy));
+        req.end();
+    });
+}
+
+function createUpstream(upstream) {
+    upstream.vhost = upstream.vhost || '/';
+    const path = `${encodeURIComponent(upstream.vhost)}/${encodeURIComponent(upstream.name)}`;
+
+    const options = {
+        method: 'PUT',
+        path: `/api/parameters/federation-upstream/${path}`,
+        auth: `${config.user}:${config.pass}`,
+        hostname: `${config.host}`,
+        port: config.adminPort,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, res => resolve(res));
+        req.on('error', err => reject(err));
+        req.write(JSON.stringify(upstream));
+        req.end();
+    });
+}
+
+function publishTo(q, action, content, options) {
+    return new Promise((resolve, reject) => {
+        const queue = config.queues.filter(arr => arr.name === q)[0].actions[action];
+
+        const exchange = findExchange(queue.source);
+        const buffer = contentToBuffer(content);
+
+        const results = this.publish(exchange.key, queue.routingKey, buffer, options);
+
+        // blocking loop
+        while (results === undefined) { console.info('broadcasting'); };
+
+        if (!results) return reject(results);
+
+        return resolve(results);
+    });
+}
+
+function queue(q, action, content, options) {
+    const queue = config.queues.filter(arr => {
+        return arr.name = q;
+    })[0];
+
+    return this.sendToQueue(`${queue.key}.${action}`, contentToBuffer(content), options);
+}
+
+function contentToBuffer(content) {
+    return (typeof content === 'object')
+        ? new Buffer(JSON.stringify(content))
+        : new Buffer(content);
+}
--- index.js
+++ index.js
@@ -1,245 +1,326 @@
 const amqp = require('amqplib');
 const http = require('http');
+const rabbitConfig = require('rabbit-config.js') || null;
 
-let config = {
-    _constructed: false,
-    user: process.env.RABBITMQ_DEFAULT_USER || null,
-    pass: process.env.RABBITMQ_DEFAULT_PASS || null,
-    host: process.env.RABBITMQ_HOST || null,
-    ssl: process.env.RABBITMQ_SSL_ENABLED || false,
-    port: process.env.RABBITMQ_PORT || 5672,
-    adminPort: process.env.RABBITMQ_ADMIN_PORT || 15672,
-    queues: [],
-    exchanges: [],
-    policies: [],
-};
 
-exports = module.exports = createRMQ.rmq = createRMQ.default = createRMQ;
+class RMQ {
+    constructor() {
+        this.protocol = (process.env.RABBITMQ_SSL_ENABLED) ? 'amqps://' : 'amqp://';
+        this.user = process.env.RABBITMQ_DEFAULT_USER || null;
+        this.pass = process.env.RABBITMQ_DEFAULT_PASS || null;
+        this.host = process.env.RABBITMQ_HOST || null;
+        this.port = process.env.RABBITMQ_PORT || 5672;
+        this.adminPort = process.env.RABBITMQ_ADMIN_PORT || 15672;
+        this.url = `${this.protocol}${this.user}:${this.pass}@${this.host}`;
+        this.queues = [];
+        this.exchanges = [];
+        this.policies = [];
+        this.upstreams = [];
+        this.existingConnection = null;
+        this.existingChannel = null;
 
-function createRMQ(cfg) {
-    const rmq = {};
-    rmq.config = initializeConfig(cfg);
+        this._configured = false;
 
-    connection()
-        .then(() => Promise.all([
-            createQueues(),
-            createExchanges(),
-            createPolicies(),
-            createUpstreams(),
-        ]))
-        .then(() => Promise.all([
-            bindQueues(),
-            createConsumers(),
-        ]))
-        .catch(err => { throw err; });
 
-    rmq.channel = createChannel;
-    rmq.connect = createConnection;
+        if (rabbitConfig) this.config(rabbitConfig);
+    }
 
-    return rmq;
-}
 
-function initializeConfig(cfg) {
-    if (!cfg) return config;
+    config(cfg) {
+        let self = this;
 
-    // setup configuration
-    config = Object.keys(cfg).reduce((acc, key) => {
-        if (key) acc[key] = cfg[key];
-
-        return acc;
-    }, config);
-
-    const protocol = (config.ssl) ? 'amqps://' : 'amqp://';
-    config.url = `${protocol}${config.user}:${config.pass}@${config.host}`;
-
-    config._constructed = true;
-    return config;
-}
-
-function createConnection(options) {
-    return amqp.connect(config.url, options)
-        .then(connection => {
-            config.connection = connection;
-            return connection;
-        })
-        .catch(err => { throw err; });
-}
-
-function createChannel() {
-    return connection()
-        .then(() => config.connection.createChannel())
-        .then(channel => {
-            channel.publishTo = publishTo;
-            channel.queue = queue;
-
-            return channel;
-        })
-        .catch(err => { throw err; });
-}
-
-function connection() {
-    return new Promise((resolve, reject) => {
-        if (config.connection) return resolve(config.connection);
-
-        return resolve(createConnection());
-    });
-}
-
-function createQueues() {
-    return createChannel()
-        .then(channel => Promise.all(config.queues.reduce((acc, q) => {
-            Object.keys(q.actions).forEach(action => {
-                acc.concat([channel.assertQueue(`${q.key}.${action}`, q.actions[action].options)]);
-            });
+        const config = Object.keys(cfg).reduce((acc, key) => {
+            if (acc[key]) acc[key] = cfg[key];
 
             return acc;
-        }, [createChannel()])))
-        .then(response => response[0].close())
-        .catch(err => { throw err; });
-}
+        }, self);
 
-function createExchanges() {
-    return createChannel()
-        .then(channel => Promise.all(config.exchanges.reduce((acc, x) => {
-            acc.concat([channel.assertExchange(x.key, x.type, x.options)]);
+        this._configured = true;
+
+        return this;
+    }
+
+
+    connection() {
+        const self = this;
+
+        return new Promise((resolve, reject) => {
+            if (self.existingConnection) return resolve(self.existingConnection);
+
+            return resolve(this._createConnection());
+        })
+        .catch(err => { throw err; });
+    }
+
+
+    close() {
+        const self = this;
+
+        return self.connection()
+            .then(connection => connection.close())
+            .then(() => self.existingConnection = null)
+            .catch(err => { throw err; });
+    }
+
+
+    connect(host) {
+        const self = this;
+
+        self.url = `${this.protocol}${this.user}:${this.pass}@${host}`;
+
+        return self._createConnection();
+
+        return
+    }
+
+
+    channel() {
+        const self = this;
+
+        return new Promise((resolve, reject) => {
+            if (self.existingChannel) return resolve(self.existingChannel);
+
+            return resolve(self._createChannel());
+        })
+        .catch(err => { throw err; });
+    }
+
+
+    create(cfg) {
+        const self = this;
+
+        if (cfg) this.config(cfg);
+
+        if (!this._configured) throw new Error('No configuration');
+
+        return this.connection()
+            .then(() => Promise.all([
+                this._createQueues(),
+                this._createExchanges(),
+                this._createPolicies(),
+                this._createUpstreams(),
+            ]))
+            .then(() => Promise.all([
+                this._bindQueues(),
+                this._createConsumers(),
+            ]))
+            .then(() => self)
+            .catch(err => { throw err; });
+    }
+
+
+    _createConnection(options) {
+        const self = this;
+
+        console.log(self.url);
+
+        return amqp.connect(this.url, options)
+            .then(connection => self.existingConnection = connection)
+            .catch(err => { throw err; });
+    }
+
+
+    _createChannel() {
+        const self = this;
+
+        return self.connection()
+            .then(connection => connection.createChannel())
+            .then(channel => {
+                self.existingChannel = channel;
+                self.existingChannel.publishTo = self.publishTo.bind({ self: self, channel });
+                self.existingChannel.queue = self.queue.bind({ self: self, channel });
+
+                return self.existingChannel;
+            })
+            .catch(err => { throw err; });
+    }
+
+
+    _createQueues() {
+        const self = this;
+
+        return this._createChannel()
+            .then(channel => Promise.all(self.queues.reduce((acc, q) => {
+                Object.keys(q.actions).forEach(action => {
+                    acc.concat([channel.assertQueue(`${q.key}.${action}`, q.actions[action].options)]);
+                });
+
+                return acc;
+            }, [self._createChannel()])))
+            .then(response => response[0].close())
+            .catch(err => { throw err; });
+    }
+
+
+    _createExchanges() {
+        const self = this;
+
+        return this._createChannel()
+            .then(channel => Promise.all(self.exchanges.reduce((acc, x) => {
+                acc.concat([channel.assertExchange(x.key, x.type, x.options)]);
+
+                return acc;
+            }, [self._createChannel()])))
+            .then(response => response[0].close())
+            .catch(err => { throw err; });
+    }
+
+
+    _createPolicies() {
+        const self = this;
+
+        return Promise.all(self.policies.reduce((acc, p) => {
+            acc.concat([self._createPolicy(p)]);
+
             return acc;
-        }, [createChannel()])))
-        .then(response => response[0].close())
+        }, []))
         .catch(err => { throw err; });
-}
+    }
 
-function createPolicies() {
-    return Promise.all(config.policies.reduce((acc, p) => {
-        acc.concat([createPolicy(p)]);
-        return acc;
-    }, []))
+
+    _createUpstreams() {
+        const self = this;
+
+        return Promise.all(self.upstreams.reduce((acc, u) => {
+            return acc.concat([self._createUpstream(u)]);
+        }, []))
         .catch(err => { throw err; });
-}
+    }
 
-function createUpstreams() {
-    return Promise.all(config.upstreams.reduce((acc, u) => {
-        acc.concat([createUpstream(u)]);
-        return acc;
-    }, []))
-        .catch(err => { throw err; });
-}
 
-function createConsumers() {
-    return createChannel()
-        .then(channel => {
-            config.queues.forEach(q => {
+    _createConsumers() {
+        const self = this;
+
+        return this._createChannel()
+            .then(channel => {
+                self.queues.forEach(q => {
+                    Object.keys(q.actions).forEach(action => {
+                        const queue = q.actions[action];
+
+                        if (queue.consume && self._findExchange(queue.source)) {
+                            channel.consume(`${q.key}.${action}`, (msg) => queue.consume(msg, channel));
+                        }
+                    });
+                });
+            })
+            .catch(err => { throw err; });
+    }
+
+
+    _findExchange(name) {
+        return this.exchanges.find(obj => obj.name === name);
+    }
+
+
+    _bindQueues() {
+        const self = this;
+
+        return this._createChannel()
+            .then(channel => Promise.all(self.queues.reduce((acc, q) => {
                 Object.keys(q.actions).forEach(action => {
                     const queue = q.actions[action];
 
-                    if (queue.consume && findExchange(queue.source)) {
-                        channel.consume(`${q.key}.${action}`, (msg) => queue.consume(msg, channel));
+                    const exchange = self._findExchange(queue.source);
+                    const queueAction = `${q.key}.${action}`;
+                    const routing = queue.pattern || queue.routingKey;
+
+                    if (exchange && !queue.noBind) {
+                        acc.concat([
+                            channel.bindQueue(queueAction, exchange.key, routing),
+                        ]);
                     }
                 });
-            });
+
+                return acc;
+            }, [self._createChannel()])))
+            .then(response => response[0].close())
+            .catch(err => { throw err; });
+    }
+
+
+    _createPolicy(policy) {
+        policy.vhost = policy.vhost || '/';
+
+        const options = {
+            method: 'PUT',
+            path: `/api/policies/${encodeURIComponent(policy.vhost)}/${encodeURIComponent(policy.name)}`,
+            auth: `${this.user}:${this.pass}`,
+            hostname: `${this.host}`,
+            port: this.adminPort,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return new Promise((resolve, reject) => {
+            const req = http.request(options, res => resolve(res));
+            req.on('error', err => reject(err));
+            req.write(JSON.stringify(policy));
+            req.end();
+        });
+    }
+
+
+    _createUpstream(upstream) {
+        upstream.vhost = upstream.vhost || '/';
+        const path = `${encodeURIComponent(upstream.vhost)}/${encodeURIComponent(upstream.name)}`;
+
+        const options = {
+            method: 'PUT',
+            path: `/api/parameters/federation-upstream/${path}`,
+            auth: `${this.user}:${this.pass}`,
+            hostname: `${this.host}`,
+            port: this.adminPort,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return new Promise((resolve, reject) => {
+            const req = http.request(options, res => resolve(res));
+            req.on('error', err => reject(err));
+            req.write(JSON.stringify(upstream));
+            req.end();
         })
         .catch(err => { throw err; });
+
+    }
+
+
+    publishTo(q, action, content, options) {
+        const { self, channel } = this;
+
+        return new Promise((resolve, reject) => {
+            const queue = self.queues.filter(arr => arr.name === q)[0].actions[action];
+
+            const exchange = self._findExchange(queue.source);
+            const buffer = self._contentToBuffer(content);
+
+            const results = this.channel.publish(exchange.key, queue.routingKey, buffer, options);
+
+            // blocking loop
+            while (results === undefined) { console.info('broadcasting'); };
+
+            if (!results) return reject(results);
+
+            return resolve(results);
+        });
+    }
+
+
+    queue(q, action, content, options) {
+        const queue = this.queues.filter(arr => arr.name = q)[0];
+
+        return this.sendToQueue(`${queue.key}.${action}`, self._contentToBuffer(content), options);
+    }
+
+    _contentToBuffer(content) {
+        return (typeof content === 'object')
+            ? new Buffer(JSON.stringify(content))
+            : new Buffer(content);
+    }
+
 }
 
-function findExchange(name) {
-    return config.exchanges.find(obj => obj.name === name);
-}
+module.exports = exports = new RMQ();
 
-function bindQueues() {
-    return createChannel()
-        .then(channel => Promise.all(config.queues.reduce((acc, q) => {
-            Object.keys(q.actions).forEach(action => {
-                const queue = q.actions[action];
-
-                const exchange = findExchange(queue.source);
-                const queueAction = `${q.key}.${action}`;
-                const routing = queue.pattern || queue.routingKey;
-
-                if (exchange && !queue.noBind) {
-                    acc.concat([
-                        channel.bindQueue(queueAction, exchange.key, routing),
-                    ]);
-                }
-            });
-
-            return acc;
-        }, [createChannel()])))
-        .then(response => response[0].close())
-        .catch(err => { throw err; });
-}
-
-function createPolicy(policy) {
-    policy.vhost = policy.vhost || '/';
-
-    const options = {
-        method: 'PUT',
-        path: `/api/policies/${encodeURIComponent(policy.vhost)}/${encodeURIComponent(policy.name)}`,
-        auth: `${config.user}:${config.pass}`,
-        hostname: `${config.host}`,
-        port: config.adminPort,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    return new Promise((resolve, reject) => {
-        const req = http.request(options, res => resolve(res));
-        req.on('error', err => reject(err));
-        req.write(JSON.stringify(policy));
-        req.end();
-    });
-}
-
-function createUpstream(upstream) {
-    upstream.vhost = upstream.vhost || '/';
-    const path = `${encodeURIComponent(upstream.vhost)}/${encodeURIComponent(upstream.name)}`;
-
-    const options = {
-        method: 'PUT',
-        path: `/api/parameters/federation-upstream/${path}`,
-        auth: `${config.user}:${config.pass}`,
-        hostname: `${config.host}`,
-        port: config.adminPort,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    return new Promise((resolve, reject) => {
-        const req = http.request(options, res => resolve(res));
-        req.on('error', err => reject(err));
-        req.write(JSON.stringify(upstream));
-        req.end();
-    });
-}
-
-function publishTo(q, action, content, options) {
-    return new Promise((resolve, reject) => {
-        const queue = config.queues.filter(arr => arr.name === q)[0].actions[action];
-
-        const exchange = findExchange(queue.source);
-        const buffer = contentToBuffer(content);
-
-        const results = this.publish(exchange.key, queue.routingKey, buffer, options);
-
-        // blocking loop
-        while (results === undefined) { console.info('broadcasting'); };
-
-        if (!results) return reject(results);
-
-        return resolve(results);
-    });
-}
-
-function queue(q, action, content, options) {
-    const queue = config.queues.filter(arr => {
-        return arr.name = q;
-    })[0];
-
-    return this.sendToQueue(`${queue.key}.${action}`, contentToBuffer(content), options);
-}
-
-function contentToBuffer(content) {
-    return (typeof content === 'object')
-        ? new Buffer(JSON.stringify(content))
-        : new Buffer(content);
-}
